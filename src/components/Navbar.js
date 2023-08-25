912bcfdc7fc3aa97956663b7cc34ab83cef012dc
kevin <bettkevin965@gmail.com>
2023-08-25 13:10:17 +0300

    Complete assignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink, useHistory } from "react-router-dom";
 const linkStyles = {
   width: "100px",
   padding: "12px",
-  margin: "0 6px 6px",
+  margin: "0 6px 7px",
   background: "blue",
   textDecoration: "none",
   color: "white",
